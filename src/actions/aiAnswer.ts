5691ae73c5ae5801268ba90d45907cb3e050ce3a
mrinal-mann <mrinal.mann24@gmail.com>
2025-03-17 21:05:45 +0530

    streaming test

--- src/actions/aiAnswer.ts
+++ src/actions/aiAnswer.ts
@@ -75,3 +75,7 @@ export async function askQuestion(question: string, projectId: string) {
     filesReferences: result,
   };
 }
+
+export const config = {
+  runtime: "edge", // Edge functions handle streaming better
+};
